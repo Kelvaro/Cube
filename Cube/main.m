bfdb4bf3479cdb174a5fb061b5b6b5ca123c6db3
Kelvaro <blizzardkang@gmail.com>
2018-02-13 23:13:37 -0800

    Initial Commit

---
+++ Cube/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cube
+//
+//  Created by BCIT Student on 2018-02-13.
+//  Copyright © 2018 BCIT Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
